71964a1ba347d785b0a513e009ebe3800336849d
Chuklin_ka <inkarttatoo@gmail.com>
2024-02-25 18:19:40 +0500

    created user models and custom super user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from sky.models import NULLABLE
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=35, verbose_name='номер телефона', **NULLABLE)
+    country = models.CharField(max_length=100, verbose_name='Страна', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from users.apps import UsersConfig
+from django.db import router
+
+
+app_name = UsersConfig.name
+
+
+urlpatterns = [
+]
